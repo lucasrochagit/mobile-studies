4c771989af691212d739be42744d55da676dfe5d
Lucas Rocha <lucascosmorocha@gmail.com>
2022-01-30 17:40:47 -0300

    feat(flutter-essencial): GridView

--- flutter/udemy/flutter-essencial/flutter_hello/lib/pages/hello_listview.dart
+++ flutter/udemy/flutter-essencial/flutter_hello/lib/pages/hello_listview.dart
@@ -16,7 +16,7 @@ class HelloListView extends StatelessWidget {
     );
   }
 
-  ListView _body() {
+  GridView _body() {
     final List<Dog> dogs = [
       Dog("Jack Russel", "assets/images/dog1.png"),
       Dog("Labrador", "assets/images/dog2.png"),
@@ -25,9 +25,44 @@ class HelloListView extends StatelessWidget {
       Dog("Pastor", "assets/images/dog5.png"),
     ];
 
-    return ListView.builder(
+    // return ListView.builder(
+    //   itemCount: dogs.length,
+    //   itemExtent: 300,
+    //   itemBuilder: (BuildContext context, int index) {
+    //     Dog dog = dogs[index];
+    //     // return _img(dog.photo);
+    //     return Stack(
+    //       fit: StackFit.expand,
+    //       children: <Widget>[
+    //         _img(dog.photo),
+    //         Align(
+    //             alignment: Alignment.topLeft,
+    //             // alignment: Alignment(1, 1),
+    //             child: Container(
+    //               margin: const EdgeInsets.all(12),
+    //               padding: const EdgeInsets.all(8),
+    //               decoration: BoxDecoration(
+    //                   color: Colors.black45,
+    //                   borderRadius: BorderRadius.circular(
+    //                     16,
+    //                   )),
+    //               child: Text(
+    //                 dog.name,
+    //                 style: const TextStyle(
+    //                   fontSize: 26,
+    //                   color: Colors.white,
+    //                 ),
+    //               ),
+    //             ))
+    //       ],
+    //     );
+    //   },
+    // );
+    return GridView.builder(
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
       itemCount: dogs.length,
-      itemExtent: 300,
+      // itemExtent: 300,
       itemBuilder: (BuildContext context, int index) {
         Dog dog = dogs[index];
         // return _img(dog.photo);
