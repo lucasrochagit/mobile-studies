83f5619f9aacd48f9fb8ef2f52abc87ba27f5918
Lucas Rocha <lucascosmorocha@gmail.com>
2022-01-30 15:56:12 -0300

    feat(flutter-essencial): ListView - Builder

--- flutter/udemy/flutter-essencial/flutter_hello/lib/pages/hello_listview.dart
+++ flutter/udemy/flutter-essencial/flutter_hello/lib/pages/hello_listview.dart
@@ -15,19 +15,31 @@ class HelloListView extends StatelessWidget {
     );
   }
 
-  Container _body() {
+  ListView _body() {
+    final List<Image> imgs = [
+      _img("assets/images/dog1.png"),
+      _img("assets/images/dog2.png"),
+      _img("assets/images/dog3.png"),
+      _img("assets/images/dog4.png"),
+      _img("assets/images/dog5.png"),
+    ];
+
+    return ListView.builder(
+      itemCount: imgs.length,
+      itemExtent: 300,
+      itemBuilder: (BuildContext context, int index) {
+        return imgs[index];
+      },
+    );
+
+    /*
     return Container(
       child: ListView(
         itemExtent: 350,
-        children: <Widget>[
-          _img("assets/images/dog1.png"),
-          _img("assets/images/dog2.png"),
-          _img("assets/images/dog3.png"),
-          _img("assets/images/dog4.png"),
-          _img("assets/images/dog5.png"),
-        ],
+        children: imgs,
       ),
     );
+    */
   }
 
   Image _img(String path) {
