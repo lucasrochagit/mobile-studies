aabb6bb6b4a568f41cd4fd34ef52d3d3c579cffa
Lucas Rocha <lucascosmorocha@gmail.com>
2022-02-03 20:33:09 -0300

    feat(flutter-essencial): [Carros] TextFormField - Decoration

--- flutter/udemy/flutter-essencial/carros/lib/pages/login_page.dart
+++ flutter/udemy/flutter-essencial/carros/lib/pages/login_page.dart
@@ -19,13 +19,37 @@ class LoginPage extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       child: ListView(
         children: <Widget>[
-          const Text("Login"),
-          TextFormField(),
-          const SizedBox(height: 10),
-          const Text("Senha"),
           TextFormField(
-            obscureText: true,
+            style: const TextStyle(
+              fontSize: 25,
+              color: Colors.blue,
+            ),
+            decoration: const InputDecoration(
+                labelText: "Login",
+                labelStyle: TextStyle(
+                  fontSize: 16,
+                ),
+                hintText: "Digite o Login",
+                hintStyle: TextStyle(
+                  fontSize: 16,
+                )),
           ),
+          const SizedBox(height: 10),
+          TextFormField(
+              obscureText: true,
+              style: const TextStyle(
+                fontSize: 25,
+                color: Colors.blue,
+              ),
+              decoration: const InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(
+                    fontSize: 16,
+                  ),
+                  hintText: "Digite a senha",
+                  hintStyle: TextStyle(
+                    fontSize: 16,
+                  ))),
           const SizedBox(height: 20),
           SizedBox(
             height: 46,
