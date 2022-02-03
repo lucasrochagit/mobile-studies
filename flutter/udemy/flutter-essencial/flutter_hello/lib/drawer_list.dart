1692f8758cecd1d770924076f28a5e4b77805c69
Lucas Rocha <lucascosmorocha@gmail.com>
2022-02-02 21:27:01 -0300

    feat(flutter-essencial): Navigation Drawer - UserAccountsDrawerHeader

--- flutter/udemy/flutter-essencial/flutter_hello/lib/drawer_list.dart
+++ flutter/udemy/flutter-essencial/flutter_hello/lib/drawer_list.dart
@@ -8,6 +8,13 @@ class DrawerList extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: <Widget>[
+          const UserAccountsDrawerHeader(
+            accountName: Text("Lucas Rocha"),
+            accountEmail: Text("lucas@mail.com"),
+            currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp3xq6yMAh7HKpnLoT17HDDvOIAJb0u98jPw&usqp=CAU")),
+          ),
           ListTile(
             leading: const Icon(Icons.star),
             title: const Text("Favoritos"),
