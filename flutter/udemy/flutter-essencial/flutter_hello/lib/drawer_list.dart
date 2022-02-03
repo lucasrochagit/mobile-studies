58dd1fb778713e633089b1b5b4667217d64e9316
Lucas Rocha <lucascosmorocha@gmail.com>
2022-02-02 21:28:46 -0300

    feat(flutter-essencial): Navigation Drawer - SafeArea

--- flutter/udemy/flutter-essencial/flutter_hello/lib/drawer_list.dart
+++ flutter/udemy/flutter-essencial/flutter_hello/lib/drawer_list.dart
@@ -5,45 +5,47 @@ class DrawerList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: <Widget>[
-          const UserAccountsDrawerHeader(
-            accountName: Text("Lucas Rocha"),
-            accountEmail: Text("lucas@mail.com"),
-            currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp3xq6yMAh7HKpnLoT17HDDvOIAJb0u98jPw&usqp=CAU")),
-          ),
-          ListTile(
-            leading: const Icon(Icons.star),
-            title: const Text("Favoritos"),
-            subtitle: const Text("Mais informações"),
-            trailing: const Icon(Icons.arrow_forward),
-            onTap: () {
-              print("Item 1");
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.help),
-            title: const Text("Ajuda"),
-            subtitle: const Text("Mais informações"),
-            trailing: const Icon(Icons.arrow_forward),
-            onTap: () {
-              print("Item 1");
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.exit_to_app),
-            title: const Text("Logout"),
-            onTap: () {
-              print("Item 1");
-              Navigator.pop(context);
-            },
-          )
-        ],
+    return SafeArea(
+      child: Drawer(
+        child: ListView(
+          children: <Widget>[
+            const UserAccountsDrawerHeader(
+              accountName: Text("Lucas Rocha"),
+              accountEmail: Text("lucas@mail.com"),
+              currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp3xq6yMAh7HKpnLoT17HDDvOIAJb0u98jPw&usqp=CAU")),
+            ),
+            ListTile(
+              leading: const Icon(Icons.star),
+              title: const Text("Favoritos"),
+              subtitle: const Text("Mais informações"),
+              trailing: const Icon(Icons.arrow_forward),
+              onTap: () {
+                print("Item 1");
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.help),
+              title: const Text("Ajuda"),
+              subtitle: const Text("Mais informações"),
+              trailing: const Icon(Icons.arrow_forward),
+              onTap: () {
+                print("Item 1");
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text("Logout"),
+              onTap: () {
+                print("Item 1");
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
