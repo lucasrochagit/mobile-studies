45d417b38166b9c9ac5abde3e04f16bf885fbdbe
Lucas Rocha <lucascosmorocha@gmail.com>
2022-02-03 20:26:11 -0300

    feat(flutter-essencial): [Carros] Layout do Form de Login

--- flutter/udemy/flutter-essencial/carros/lib/pages/login_page.dart
+++ flutter/udemy/flutter-essencial/carros/lib/pages/login_page.dart
@@ -16,6 +16,35 @@ class LoginPage extends StatelessWidget {
 
   _body() {
     return Container(
+      padding: const EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          const Text("Login"),
+          TextFormField(),
+          const SizedBox(height: 10),
+          const Text("Senha"),
+          TextFormField(
+            obscureText: true,
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            height: 46,
+            child: ElevatedButton(
+              child: const Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+              onPressed: () => {},
+              style: TextButton.styleFrom(
+                primary: Colors.blue,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
