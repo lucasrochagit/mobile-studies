518f4710aca78dc257de3d8fda2c61e7bb33cff0
Lucas Rocha <lucascosmorocha@gmail.com>
2022-02-03 22:12:01 -0300

    feat(flutter-essencial): [Carros] Web Servce (application/json)

--- flutter/udemy/flutter-essencial/carros/lib/login_api.dart
+++ flutter/udemy/flutter-essencial/carros/lib/login_api.dart
@@ -1,14 +1,21 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class LoginApi {
   static Future<bool> login(String login, String senha) async {
-    final Uri url = Uri.parse('http://livrowebservices.com.br/rest/login');
+    final Uri url =
+        Uri.parse('http://carros-springboot.herokuapp.com/api/v2/login');
     final Map body = {
-      'login': login,
-      'senha': senha,
+      'username': login,
+      'password': senha,
     };
 
-    var response = await http.post(url, body: body);
+    final Map<String, String> headers = {
+      'Content-Type': 'application/json'
+    };
+
+    var response = await http.post(url, body: json.encode(body), headers: headers);
 
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
