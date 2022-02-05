516fe87c260b7e462845133ced1603a011752730
Lucas Rocha <lucascosmorocha@gmail.com>
2022-02-05 18:52:03 -0300

    feat(flutter-essencial): [Carros] Layout Com Card e ButtonBarTheme

--- flutter/udemy/flutter-essencial/carros/lib/pages/home_page.dart
+++ flutter/udemy/flutter-essencial/carros/lib/pages/home_page.dart
@@ -21,30 +21,67 @@ class HomePage extends StatelessWidget {
   _body() {
     List<Carro> carros = CarrosApi.getCarros();
 
-    return ListView.builder(
-        // itemCount: carros.length,
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          Carro c = carros[index];
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: ListView.builder(
+          // itemCount: carros.length,
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            Carro c = carros[index];
 
-          return Row(
-            children: <Widget>[
-              Image.network(
-                c.urlFoto!,
-                width: 150,
-              ),
-              Flexible(
-                child: Text(
-                  c.nome!,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontSize: 22,
-                  ),
+            return Card(
+              color: Colors.grey[100],
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Center(
+                      child: Image.network(
+                        c.urlFoto!,
+                        width: 250,
+                      ),
+                    ),
+                    Text(
+                      c.nome!,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 22,
+                      ),
+                    ),
+                    const Text(
+                      'Descrição....',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    ButtonBarTheme(
+                      data: ButtonBarTheme.of(context),
+                      child: ButtonBar(
+                        children: <Widget>[
+                          TextButton(
+                            child: const Text('DETALHES'),
+                            onPressed: () {
+                              /* ... */
+                            },
+                          ),
+                          const SizedBox(width: 8),
+                          TextButton(
+                            child: const Text('COMPARTILHAR'),
+                            onPressed: () {
+                              /* ... */
+                            },
+                          ),
+                          const SizedBox(width: 8),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ],
-          );
-        });
+            );
+          }),
+    );
   }
 }
