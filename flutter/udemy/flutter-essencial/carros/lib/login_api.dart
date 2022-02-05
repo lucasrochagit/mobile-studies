67e7719737a4d2902fff69fb4dbc7481cabd9835
Lucas Rocha <lucascosmorocha@gmail.com>
2022-02-05 17:20:44 -0300

    feat(flutter-essencial): [Carros] Parser de JSON - Simples

--- flutter/udemy/flutter-essencial/carros/lib/login_api.dart
+++ flutter/udemy/flutter-essencial/carros/lib/login_api.dart
@@ -20,6 +20,13 @@ class LoginApi {
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
 
+    Map mapResponse = json.decode(response.body);
+
+    String nome = mapResponse['nome'];
+    String email = mapResponse['email'];
+
+    print("Nome $nome, email $email");
+
     return response.statusCode == 200;
   }
 }
