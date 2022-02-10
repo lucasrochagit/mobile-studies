31f892f8468cd47cd73c3dbe74bcdf51196f7fd3
Lucas Rocha <lucascosmorocha@gmail.com>
2022-02-10 20:55:53 -0300

    feat(flutter-essencial): Actions: IconButton e PopupMenuButton

--- flutter/udemy/flutter-essencial/carros/lib/pages/carro_page.dart
+++ flutter/udemy/flutter-essencial/carros/lib/pages/carro_page.dart
@@ -1,5 +1,4 @@
 import 'package:carros/models/carro.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CarroPage extends StatelessWidget {
@@ -13,6 +12,38 @@ class CarroPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(carro.nome ?? 'Carro'),
         centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            onPressed: _onClickMap(),
+            icon: const Icon(
+              Icons.place,
+            ),
+          ),
+          IconButton(
+            onPressed: _onClickVideo(),
+            icon: const Icon(
+              Icons.videocam,
+            ),
+          ),
+          PopupMenuButton(
+              onSelected: _onClickPopupMenu,
+              itemBuilder: (context) {
+                return [
+                  const PopupMenuItem(
+                    child: Text("Editar"),
+                    value: "Editar",
+                  ),
+                  const PopupMenuItem(
+                    child: Text("Deletar"),
+                    value: "Deletar",
+                  ),
+                  const PopupMenuItem(
+                    child: Text("Compartilhar"),
+                    value: "Compartilhar",
+                  )
+                ];
+              })
+        ],
       ),
       body: _body(),
     );
@@ -21,7 +52,28 @@ class CarroPage extends StatelessWidget {
   _body() {
     return Container(
       padding: const EdgeInsets.all(16),
-      child: Image.network(carro.urlFoto ?? 'https://www.inovegas.com.br/site/wp-content/uploads/2017/08/sem-foto.jpg'),
+      child: Image.network(carro.urlFoto ??
+          'https://www.inovegas.com.br/site/wp-content/uploads/2017/08/sem-foto.jpg'),
     );
+  }
+
+  _onClickMap() {}
+
+  _onClickVideo() {}
+
+  _onClickPopupMenu(String value) {
+    switch (value) {
+      case "Editar":
+        print("Editar!!");
+        break;
+      case "Deletar":
+        print("Deletar!!");
+        break;
+      case "Compartilhar":
+        print("Compartilhar!!");
+        break;
+      default:
+        break;
+    }
   }
 }
