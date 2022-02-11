130d49d14020df60f514a29f93ea9cbf08da55e1
Lucas Rocha <lucascosmorocha@gmail.com>
2022-02-10 21:44:00 -0300

    feat(flutter-essencial): Cache - LoripsumApi

--- flutter/udemy/flutter-essencial/carros/lib/bloc/loripsum_bloc.dart
+++ flutter/udemy/flutter-essencial/carros/lib/bloc/loripsum_bloc.dart
@@ -3,8 +3,11 @@ import 'package:carros/bloc/simple_bloc.dart';
 import 'package:carros/external/loripsum_api.dart';
 
 class LoripsumBloc extends SimpleBloc<String> {
+  static String? lorim;
+
   fetch() async {
-    String s = await LoripsumApi.getLoripsum();
+    String s = lorim ?? await LoripsumApi.getLoripsum();
+    lorim = s;
     add(s);
   }
 }
