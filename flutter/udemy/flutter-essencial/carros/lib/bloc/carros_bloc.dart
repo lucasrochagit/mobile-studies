8cc11c9ba9590501fb8afa7f830a0d0a35fd6b4c
Lucas Rocha <lucascosmorocha@gmail.com>
2022-02-20 12:44:07 -0300

    feat(flutter-essencial): Cache com banco de dados

--- flutter/udemy/flutter-essencial/carros/lib/bloc/carros_bloc.dart
+++ flutter/udemy/flutter-essencial/carros/lib/bloc/carros_bloc.dart
@@ -1,12 +1,20 @@
 import 'dart:async';
 
 import 'package:carros/bloc/simple_bloc.dart';
+import 'package:carros/db/dao/carro_dao.dart';
 import 'package:carros/external/carros_api.dart';
 import 'package:carros/models/carro.dart';
+import 'package:carros/utils/network.dart';
 
 class CarrosBloc extends SimpleBloc<List<Carro>> {
   Future<List<Carro>?> fetch(TipoCarro tipo) async {
     try {
+      bool networkOn = await isNetworkOn();
+
+      if (!networkOn) {
+        return await CarroDAO().findAllByTipo(tipo.name);
+      }
+
       List<Carro> carros = await CarrosApi.getCarros(tipo);
       add(carros);
       return carros;
